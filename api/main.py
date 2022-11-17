3b5bfc3a5816a8cde07af9c818eb8e62c7982810
Aomaf <Aomaf08@gmail.com>
2022-11-17 11:45:02 +0200

    Recive F_image from clinet and prerocess and show

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
 import numpy as np
-from io import BytesIO
 from PIL import Image
 import uvicorn
 import tensorflow as tf
@@ -14,46 +13,50 @@ async def bing():
     return "Hello, the server is alive"
 
 
-
-
-
 def display_image(image):
-   print(image.shape)
-   print(image)
+#  print(image.shape) 
    cv2.imshow("test image", image)
    cv2.waitKey(0)
    cv2.destroyAllWindows()
     
-def read_files_as_images(iamge_bytes, mask_bytes):
+def read_files_as_images(iamge_bytes):
    image_file = cv2.imdecode(np.frombuffer(iamge_bytes, dtype = np.uint8), cv2.IMREAD_ANYCOLOR)
-   mask_file = cv2.imdecode(np.frombuffer(mask_bytes, dtype = np.uint8), cv2.IMREAD_GRAYSCALE)
-   display_image(mask_file)
+#    display_image(image_file)
+#    print(image_file)
+#    print(image_file.shape) #(512, 512, 3)
+   return image_file
 
-   return image_file, mask_file
-
-def Image_mask_input_preprocess(image_file, mask_file):
+def Image_input_preprocess(image_file):
     x = np.zeros((1, 256, 256, 3), dtype=np.float32)
-    y = np.zeros((1, 256, 256, 1), dtype=np.float32)
     image = cv2.resize(image_file, (256, 256))
-    mask = cv2.resize(mask_file, (256, 256))
-    mask = np.reshape(mask, (256, 256, 1))
+    # print(image.shape)
     x[0] = image
-    y[0] = mask     
     image = image / 255
-    mask = mask / 255
-    return image, mask
+    # print(image.shape)
+    return image
+
+def prediction_output_Image_processing(img):
+    print(img)
+    im = cv2.imwrite("result Image", img)
+    cv2.imshow('image',im)
+    cv2.waitKey(0)
 
 @app.post("/predict")
-async def predect(files: list[UploadFile]= File(...)):
-    uploaded_image_bytes = await files[0].read()
-    uploaded_mask_bytes = await files[1].read()
-    image_file, mask_file = read_files_as_images(uploaded_image_bytes,uploaded_mask_bytes) 
-    pass
+async def predect(file: UploadFile= File(...)):
+    uploaded_image_bytes = await file.read()
+    image_file = read_files_as_images(uploaded_image_bytes) 
+    image_batch = Image_input_preprocess(image_file) #preparing image as a batch by adding a diminsion to be provided to the model.predict
+    model_image = np.expand_dims(image_batch,0)
+    prediction_res = MODEL.predict(model_image)
+    # p_reshape = np.squeeze(prediction_res, axis=0)
+    cv2.imshow('iamge', prediction_res[0].astype(np.uint8)*255)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    image_batch = Image_mask_input_preprocess(image_file, mask_file) #preparing image as a batch by adding a diminsion to be provided to the model.predict
-    prediction_res = MODEL.predict(image_batch)
-    return prediction_res
-    pass
+    # prediction_output_Image_processing(prediction_res)
+    
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8080)
 
