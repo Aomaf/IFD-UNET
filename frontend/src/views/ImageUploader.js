c638d50fa10d5d5aa4e9a95e975bc4b80393aa0c
Aomaf <Aomaf08@gmail.com>
2022-11-17 11:45:59 +0200

    Recive Image > Preprocess > display

---
+++ frontend/src/views/ImageUploader.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import axios from "../axios";
+
+const [selectedFile, setSelectedFile] = useState(null);
+const OnFileChange = () => {};
+
+function ImageUploader() {
+  useEffect(() => {
+    async function loadImage() {
+      const predictedMask = imageaxios.get("");
+    }
+  }, []);
+}
